2247554f7dc1a520d810181462aa9dae5048a7cd
Saiteja Bingi <saiteja1973b@gmail.com>
2026-01-04 22:40:26 +0530

    implemented the routers

--- server.js
+++ server.js
@@ -8,21 +8,77 @@ const app=exp()
 const port=8080
 app.listen(port,()=>console.log(`server listening on port${port}`))
 
+
+// add body parser middleware
+app.use(exp.json())
+
+// test-data
+    let users=[]
+
 // Create user API
     // route to handle GET req
     app.get('/users',(req,res)=>{
         // send response to client
-        res.json({message:"all user data"})
+        // read get useres and send res
+        res.json({message:"all users ",payload:users})
     })
+
+    // route to send a user by id
+    app.get("/users/:id",(req,res)=>{
+        // read id from url
+        let userId=Number(req.params.id)
+        // find user by id
+        let user =users.find(userObj=>userObj.id===userId)
+        // if user not found
+
+        if(user===undefined){
+            res.json({message:"user not found"})
+        }
+        else{
+            res.json({message:"user found",payload:user})
+        }
+    })
+
+
     // route to handle POST req
     app.post('/user',(req,res)=>{
+
+        // get new user from req;
+        let newUser=req.body
+        console.log("newuser is: ",newUser)
+        users.push(newUser)
+        // send res
         res.json({message:'user created'})
     })
+
+
     // route to handle PUT req
     app.put('/user',(req,res)=>{
-        res.json({message:'user MODIFIED'})
+        // get modifieed user
+        let modifiedUser=req.body
+        console.log("newuser is: ",modifiedUser)
+        let index=users.findIndex(userObj=>userObj.id===modifiedUser.id)
+        if(index==-1){
+            res.json({message:"user not found to update"})
+        }
+        else{
+            users.splice(index,1,modifiedUser)
+            res.json({message:"user modified"})
+        }
     })
+
     // route to handle DELETE req
-    app.delete('/user',(req,res)=>{
-        res.json({message:'user deleted'})
+    app.delete('/user:id',(req,res)=>{
+        // read id from url parameter
+        let userId=Number(req.params.id)
+        let index=users.findIndex(userObj=>userObj.id==userId)
+        if(index==-1){
+            res.json({message:"user not found to update"})
+        }
+        else{
+            users.splice(index,1)
+            res.json({message:"user deleted"})
+        }
     })
+
+
